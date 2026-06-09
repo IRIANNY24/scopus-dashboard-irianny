5dbd27e94f74dc293920c8ce740c7690a7d371e7
IRIANNY24 <iriannisardila@gmail.com>
2026-06-08 20:32:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="FraudML Analytics", layout="wide")
 
-st.title("🔍 FraudML Analytic
+st.title("🔎 FraudML Analytics")
 st.write("Detección de Fraudes usando Machine Learning")
 
 st.subheader("Pregunta de Investigación")
